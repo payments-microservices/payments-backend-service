1c4618ff6d52544410792f09943449073687ab28
Amisha-Sinha <83289237+Amisha-Sinha@users.noreply.github.com>
2025-03-22 15:28:56 +0530

    Update AccountRepository.java

--- backend-service/src/main/java/com/payments/backendservice/repository/AccountRepository.java
+++ backend-service/src/main/java/com/payments/backendservice/repository/AccountRepository.java
@@ -5,5 +5,5 @@ import com.payments.backendservice.collection.Account;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AccountRepository extends MongoRepository<Account, String> {
-    Account findByAccountNumber(String userName);
+    Account findByAccountNumber(String accountNumber);
 }
